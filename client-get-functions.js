64eabd34207177b084218976c452657612065969
Gaston Alasia <galasia@itba.edu.ar>
2023-09-24 18:22:52 -0300

    get funtion test
    Co-Authored-By: Matías Della Torre <72523405+matidellatorre@users.noreply.github.com>

--- client-get-functions.js
+++ client-get-functions.js
@@ -1,5 +1,12 @@
+const pool = require("./db")
+const queries = require("./queries")
+
 const getAllClients = (req, res) => {
-    res.status(200).send('OK')
+    pool.query(queries.getAllClientsQuery, (error, results) => {
+        if (error) throw error
+        else
+        res.status(200).json(results.row)
+    })
 }
 
 module.exports = {
---
+++ queries.js
@@ -0,0 +1,5 @@
+const getAllClientsQuery = "SELECT * FROM e01_cliente"
+
+module.exports = {
+    getAllClientsQuery
+}
