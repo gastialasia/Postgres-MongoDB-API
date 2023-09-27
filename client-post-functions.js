8ba29add0c4bbcbdbba003b2100063d50929e908
Matías Della Torre <matidellatorre@gmail.com>
2023-09-27 14:27:36 -0300

    Modify client por body

--- client-post-functions.js
+++ client-post-functions.js
@@ -27,40 +27,25 @@ const removeClient = (req, res) => {
 }
 
 const modifyClient = (req, res) => {
-    const nro_cliente = req.query.nro_cliente
-    delete req.query.nro_cliente
+    const client_id = req.query.client_id
+    //delete req.query.client_id
 
-    const queryParamsArray = Object.entries(req.query)
+    const queryParamsArray = Object.entries(req.body)
     //console.log(nro_cliente)
     //console.log(queryParamsArray.length)
     //console.log(queryParamsArray)
     //const len = queryParamsArray.length
-    const query = queries.buildModifyString(queryParamsArray, nro_cliente)
-    const body = req.body
-    var failed = false
+    const query = queries.buildModifyString(queryParamsArray, client_id)
 
     console.log(query)
 
     pool.query(query, (error, results) => {
         if (error) {
-            failed = true
+            res.status(400).send("Error en la BD: " + error.message)
+        } else {
+            res.status(201).send("Parametro del cliente modificado con exito")
         }
     })
-
-    /*for (const key in body) {
-        if (body.hasOwnProperty(key)) {
-            const value = body[key];
-            console.log(`key: ${key}, value: ${value}`);
-            pool.query(queries.modifyUserQuery, [client_id, key, value], (error, results) => {
-                if (error) {
-                    failed = true
-                }
-            })
-        }
-    }*/
-    if (!failed) {
-        res.status(201).send("Parametro del cliente modificado con exito")
-    }
 }
 
 module.exports = {
