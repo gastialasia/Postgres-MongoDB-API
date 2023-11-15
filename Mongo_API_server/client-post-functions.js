d501a8802a737ef6c390d5387533f323426fa00e
Gaston Alasia <galasia@itba.edu.ar>
2023-11-14 23:10:41 -0300

    Mongo API CRUD operations finished
    Co-Authored-By: Matías Della Torre <72523405+matidellatorre@users.noreply.github.com>

--- Mongo_API_server/client-post-functions.js
+++ Mongo_API_server/client-post-functions.js
@@ -8,35 +8,22 @@ const addClient = async (req, res) => {
     res.send(result).status(204);
 }
 
-const removeClient = (req, res) => {
-    const client_id = req.query.client_id
-    pool.query(queries.removeUserQuery, [client_id], (error, results) => {
-        if (error) {
-            res.status(400).send("Error en la BD: " + error.message)
-        }
-        else {
-            console.log(client_id)
-            res.status(201).send("Cliente removido con exito")
-        }
-    })
+const removeClient = async (req, res) => {
+    const client_id = parseInt(req.query.client_id);
+    const query = {nro_cliente:client_id};
+    console.log(query)
+    let collection = await database.collection("clientes");
+    let result = await collection.deleteOne(query);
+    res.send(result).status(200);
 }
 
-const modifyClient = (req, res) => {
-    const client_id = req.query.client_id
-
-    const queryParamsArray = Object.entries(req.body)
-
-    const query = queries.buildModifyString(queryParamsArray, 'e01_cliente', 'nro_cliente', client_id)
-
-    console.log(query)
-
-    pool.query(query, (error, results) => {
-        if (error) {
-            res.status(400).send("Error en la BD: " + error.message)
-        } else {
-            res.status(201).send("Parametro/s del cliente modificado con exito")
-        }
-    })
+const modifyClient = async (req, res) => {
+    const client_id = parseInt(req.query.client_id);
+    let collection = await database.collection("clientes");
+    let params = req.body;
+    let query = {nro_cliente:client_id};
+    let result = await collection.updateOne(query,{$set:params});
+    res.send(result).status(200);
 }
 
 module.exports = {
--- Mongo_API_server/product-post-functions.js
+++ Mongo_API_server/product-post-functions.js
@@ -8,35 +8,22 @@ const addProduct = async (req, res) => {
     res.send(result).status(204);
 }
 
-const removeProduct = (req, res) => {
-    const product_id = req.query.product_id
-    pool.query(queries.removeProductQuery, [product_id], (error, results) => {
-        if (error) {
-            res.status(400).send("Error en la BD: " + error.message)
-        }
-        else {
-            res.status(201).send("Producto removido con exito")
-        }
-    })
+const removeProduct = async (req, res) => {
+    const product_id = parseInt(req.query.client_id);
+    const query = {codigo_producto:product_id};
+    console.log(query)
+    let collection = await database.collection("producto");
+    let result = await collection.deleteOne(query);
+    res.send(result).status(200);
 }
 
-const modifyProduct = (req, res) => {
-
-    const product_id = req.query.product_id
-
-    const queryParamsArray = Object.entries(req.body)
-
-    const query = queries.buildModifyString(queryParamsArray, 'e01_producto', 'codigo_producto', product_id)
-
-    console.log(query)
-
-    pool.query(query, (error, results) => {
-        if (error) {
-            res.status(400).send("Error en la BD: " + error.message)
-        } else {
-            res.status(201).send("Parametro/s del producto modificado con exito")
-        }
-    })
+const modifyProduct = async (req, res) => {
+    const product_id = parseInt(req.query.product_id);
+    let collection = await database.collection("producto");
+    let params = req.body;
+    let query = {codigo_producto:product_id};
+    let result = await collection.updateOne(query,{$set:params});
+    res.send(result).status(200);
 }
 
 module.exports = {
